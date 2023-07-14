dcbb2ac122c44e1452d428856808007cae152d43
issamelferkh <issam.elferkh@gmail.com>
2023-07-14 16:38:42 +0200

    fixing webpack, bcrypt, aws-sdk, mock-aws-s3 and nock issue

--- client/src/Component/SignIn/SignIn.js
+++ client/src/Component/SignIn/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import logo from './user-signin.png';
 
 function SignIn() {
